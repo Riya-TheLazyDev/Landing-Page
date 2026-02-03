d462fbcb26c560c7f3d908013f4989f5bf8da5a7
Riya <riyarana.rohtak@gmail.com>
2026-02-03 23:14:48 +0530

    Added Shadcn and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Timer } from "lucide-react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Riya Rana</h1>
+
+      <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl border border-slate-900 shadow-md" onClick={() => setCount((count) => count + 1)}>
+        <Timer className="mr-2 h-4 w-4" />
+        Count is {count}
+      </Button>
+    </div>
+  );
+}
+
+export default App;
